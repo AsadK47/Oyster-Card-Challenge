a9ffb17ce8bb6e1d4dc55af763b170c4ff827f54
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-05 18:46:29 +0000

    Step 8 - Complete

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,21 +1,36 @@
 class OysterCard
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   DEFAULT_BALANCE = 0
-  LIMIT = 90
+  MAXLIMIT = 90
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
-    fail "Error: Card limit is #{LIMIT}" if @balance + amount > 90
+    fail "Error: Card MAX limit is #{MAXLIMIT}" if @balance + amount > 90
     @balance += amount
   end
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    fail "You can't touch in twice!" if @in_journey == true
+    @in_journey = true
+  end
+
+  def touch_out
+    fail "You can't touch out twice!" if @in_journey == false
+    @in_journey = false
   end
 
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -7,9 +7,7 @@ describe OysterCard do
 
   describe "#initialize with balance of 0" do
 
-    it 'responds to #balance' do
-      expect(subject).to respond_to :balance
-    end
+    it { is_expected.to respond_to(:balance) }
 
     it 'has a default balance of 0' do
       expect(subject.balance).to eq OysterCard::DEFAULT_BALANCE
@@ -19,18 +17,16 @@ describe OysterCard do
 
   describe '#top_up' do
 
-    it 'responds to #top_up' do
-      expect(subject).to respond_to(:top_up).with(1).argument
-    end
+    it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it "Can top up balance" do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
 
-    it "Can't top_up more than balance" do
-      max = OysterCard::LIMIT
+    it "Can't top_up more than LIMIT" do
+      max = OysterCard::MAXLIMIT
       subject.top_up(max)
-      expect{ subject.top_up 1 }.to raise_error "Error: Card limit is #{OysterCard::LIMIT}"
+      expect{ subject.top_up 1 }.to raise_error "Error: Card MAX limit is #{OysterCard::MAXLIMIT}"
     end
 
   end
@@ -39,12 +35,48 @@ describe OysterCard do
 
     it { is_expected.to respond_to(:deduct).with(1).argument }
 
-    it "Return a value instead of nil" do
-      expect(subject.deduct 1).to be_a(Numeric)
-    end
-
     it "Deducts amount spent" do
       expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
+    end
+
+  end
+
+  describe "#in_journey" do
+
+    it { is_expected.to respond_to(:in_journey?) }
+
+    it "Initializes with in_journey? as false" do
+      expect(card.in_journey?).to eq false
+    end
+
+  end
+
+  describe "#touch_in" do
+
+    it { is_expected.to respond_to(:touch_in)}
+
+    it "Changes the state of in_journey? to true" do
+      expect{ card.touch_in }.to change{ card.in_journey? }.to(true)
+    end
+
+    it "Checks if card is already in_journey and prevents double touch" do
+      card.touch_in
+      expect{ card.touch_in }.to raise_error "You can't touch in twice!"
+    end
+
+  end
+
+  describe "#touch_out" do
+
+    it { is_expected.to respond_to(:touch_out) }
+
+    it "Changes the state of in_journey? to false" do
+      card.touch_in
+      expect { card.touch_out }.to change{ card.in_journey? }.to(false)
+    end
+
+    it "Checks if card is already out and prevents touch_out" do
+      expect{ card.touch_out }.to raise_error "You can't touch out twice!"
     end
 
   end
